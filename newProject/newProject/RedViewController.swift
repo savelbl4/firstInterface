935d1174b9289be78cd0640d840aae60728759b5
save_bl4 <savelbl4@gmail.com>
2019-03-06 21:52:30 +0300

    добавил три желтых штуки

--- newProject/newProject/RedViewController.swift
+++ newProject/newProject/RedViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 class RedViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     var login: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         
         print(login)
     }
-
-
 }
 
+extension RedViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let yellowCell = YellowTableViewCell()
+        yellowCell.contentView.backgroundColor = .yellow
+        return yellowCell
+    }
+    
+    
+}
